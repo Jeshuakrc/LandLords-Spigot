2d8f6e3dbe4be10adfe0a70652a3327d6819dfb3
Jeshua Reyes <jeshuakrc@gmail.com>
2022-04-26 19:51:00 -0600

    Using new AbilityTriggeredEvent from RegionsLib to show Ability Denied messages once again.

--- src/main/java/com/jkantrell/landlords/region/LandlordsRegionListeners.java
+++ src/main/java/com/jkantrell/landlords/region/LandlordsRegionListeners.java
@@ -3,9 +3,13 @@ package com.jkantrell.landlords.region;
 import com.jkantrell.landlords.Landlords;
 import com.jkantrell.landlords.io.Config;
 import com.jkantrell.landlords.io.LangManager;
+import com.jkantrell.regionslib.events.AbilityTriggeredEvent;
 import com.jkantrell.regionslib.events.PlayerEnterRegionEvent;
 import com.jkantrell.regionslib.events.PlayerLeaveRegionEvent;
 import com.jkantrell.regionslib.regions.Region;
+import net.md_5.bungee.api.ChatMessageType;
+import net.md_5.bungee.api.chat.TextComponent;
+import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -32,5 +36,40 @@ public class LandlordsRegionListeners implements Listener {
     @EventHandler
     private void onPlayerEnterRegion(PlayerLeaveRegionEvent e) {
         //Nothing here
+    }
+
+    @EventHandler
+    private void onAbilityTriggered(AbilityTriggeredEvent e) {
+        if (e.isAllowed()) { return; }
+
+        Player player = e.getPlayer();
+        String  regionName = e.getRegion().getName(),
+                abilityName = e.getAbility().getName().toLowerCase();
+
+        try {
+            String  message,
+                    path = "action_denied." + abilityName.toLowerCase();
+            try {
+                message = LangManager.getString(path, player, regionName);
+            } catch (NullPointerException ex) {
+                message = LangManager.getString("action_denied.default", player, regionName);
+                Bukkit.getLogger().warning(String.format(
+                        """
+                            %1$s doesn't have the ability "%2$s" in the region "%3$s", but a specific denial message wasn't found in the "%4$s" lang file.
+                            Displaying the default denied action message.
+                            Include the "action_denied.%2$s" entry in the lang file to provide an specific message.""",
+                        player.getName(), abilityName, regionName, LangManager.getLangFileName(player)
+                ));
+            }
+            if (message.equals("")) { return; }
+            player.spigot().sendMessage(ChatMessageType.ACTION_BAR, TextComponent.fromLegacyText(message));
+        } catch (NullPointerException ex) {
+            Bukkit.getLogger().warning(String.format(
+                    """
+                            %1$s doesn't have the ability "%2$s" in the region "%3$s", but neither a specific nor default denial message was found in the "%4$s" lang file.
+                            Make sure to include the "action_denied.%2$s" or "action_denier.default" entry in the lang file.""",
+                    player.getName(), abilityName, regionName, LangManager.getLangFileName(player)
+            ));
+        }
     }
 }
